8f68b70edff24d3bc05632ace0d7b736300419fe
ray-abel12 <rayabel12@outlook.com>
2020-05-03 18:30:14 +0100

    update

---
+++ course.ts
@@ -0,0 +1,20 @@
+var x :number;
+x=21;
+
+x;
+console.log(x)
+var flag:boolean=true;
+
+console.log(flag);
+
+var firstName:string='abel';
+console.log(firstName);
+var demo:any='hi).1234.';
+console.log(demo);
+
+var num :number[]=[1,23,4,56];
+num;
+
+var person:any=[{id:1,name:'abel',age:24},{id:2,name:'ray',age:25}]
+person
+
